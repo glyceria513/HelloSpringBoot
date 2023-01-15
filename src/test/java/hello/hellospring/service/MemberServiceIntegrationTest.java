312f4ccac34ae175c07f50afc5f28e80bc640289
Haru1 <glyceria622@naver.com>
2023-01-15 21:18:59 +0900

    DB접근기술사용-순수JDBC로 변경 & 통합테스트코드 작성

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,71 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    void 회원가입() {
+        //given : 테스트할 데이터
+        Member member = new Member();
+        member.setName("해리");
+
+        //when : 회원가입실행
+        Long saveId = memberService.join(member);
+
+        //then : 저장소에 저장된 name과 생성한 member객체의 name이 같은지 검증
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 회원가입_중복예외검증() {
+        Member member1 = new Member();
+        member1.setName("해리");
+        Member member2 = new Member();
+        member2.setName("해리");
+
+        memberService.join(member1);
+        // memberService.join(member2)를 수행한 결과가 IllegalStateException 이면 테스트는 성공한다.
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        
+        //에러메시지 검증
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        
+//        //member1, member2의 name이 같으므로 예외가 발생해야한다. 예외가 발생하면 테스트는 성공한다.
+//        //but, 문제가 생겼는지 아닌지 알 수 없어 애매하므로 assert문을 통해 검증을 수행해준다.
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
